97815f9f4bc2ba9c271ac97031893ab5c3d36641
Mouzayan Delbourgo <mouzayan@arcade.xyz>
2025-02-08 17:06:24 -0500

    feat(gp-dynamic-fees): first commit

---
+++ src/GasPriceFeesHook.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.6;
+
+import {BaseHook} from "v4-periphery/src/base/hooks/BaseHook.sol";
+import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
+import {Hooks} from "v4-core/libraries/Hooks.sol";
+import {PoolKey} from "v4-core/types/PoolKey.sol";
+import {BalanceDelta} from "v4-core/types/BalanceDelta.sol";
+import {LPFeeLibrary} from "v4-core/libraries/LPFeeLibrary.sol";
+import {BeforeSwapDelta, BeforeSwapDeltaLibrary} from "v4-core/types/BeforeSwapDelta.sol";
+
+contract GasPriceFeesHook is BaseHook {
+    using LPFeeLibrary for uint24;
+}
